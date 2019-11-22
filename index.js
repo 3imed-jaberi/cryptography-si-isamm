3794ccab5d934a6b160c2e4f6f3f97019520a083
3imed-jaberi <imed_jebari@hotmail.fr>
2019-11-22 20:42:39 +0100

    entry point <main> 🎯 ..

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
-// init package code 🚀 .. 
+const Cesar = require('./src/cryptography/Cesar');
+const Affine = require('./src/cryptography/Affine');
+const Vigenere = require('./src/cryptography/Vigenere');
+const Transposition = require('./src/cryptography/Transposition');
+
+
+console.log(Transposition('demain a quatorze heure','maman','encrypt'));
+
+console.log(Transposition('/E AER/A OH /DNUZU/MAT E/IQRE ','maman','decrypt'));
+
+module.exports = {
+  Cesar,
+  Affine,
+  Vigenere,
+  Transposition
+};
