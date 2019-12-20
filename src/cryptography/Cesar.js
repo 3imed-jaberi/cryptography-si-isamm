e31595024dfcdf4af89af8468c90ec7e59d9a33b
3imed-jaberi <imed_jebari@hotmail.fr>
2019-12-20 17:44:13 +0100

    add params checksum --rock 🧲..

--- src/cryptography/Cesar.js
+++ src/cryptography/Cesar.js
@@ -3,6 +3,7 @@ const {
   __LENGTH_ALPHABET__,
   __MAJUS_CODE_OF_A__ 
 } = require('../global');
+const { isString, isNumber, hasMaxLength } = require('../validation');
 
 
 
@@ -13,7 +14,13 @@ const {
  * @param {string} message : The text you want to encrypt / decrypt .. 
  * @param {number} key : key for encrypt / decrypt text ..  
  */
-const main = (message, key) => message.toUpperCase().match(__STRING_ALPHABET_RegExp__).map( letter => String.fromCharCode((((letter.charCodeAt(0) - __MAJUS_CODE_OF_A__) + key) % __LENGTH_ALPHABET__ ) + __MAJUS_CODE_OF_A__ )).join('');
+const main = (message, key) => {  
 
+  if(isString(message) && isNumber(key) && hasMaxLength(key,2)){
+    return message.toUpperCase().match(__STRING_ALPHABET_RegExp__).map( letter => String.fromCharCode((((letter.charCodeAt(0) - __MAJUS_CODE_OF_A__) + key) % __LENGTH_ALPHABET__ ) + __MAJUS_CODE_OF_A__ )).join('');
+  }else{
+    throw new Error('Check you inputs .. ');
+  }
+}
 
 module.exports = main ;
