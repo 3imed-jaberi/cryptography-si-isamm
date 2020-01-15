e2a6767593503c1f3a0aa797610b901b30892bb4
3imed-jaberi <imed_jebari@hotmail.fr>
2020-01-15 18:27:30 +0100

    add encrypt/decrypt const 🩰..

--- src/global/index.js
+++ src/global/index.js
@@ -3,6 +3,8 @@ const __STRING_ALPHABET_RegExp__ = /[A-Z]/g;
 const __CONST_SEPARATOR__ = '/' ;
 const __LENGTH_ALPHABET__ = 26;
 const __MAJUS_CODE_OF_A__ = 65;
+const __DECRYPT__ = 'decrypt';
+const __ENCRYPT__ = 'encrypt';
 
 
 
@@ -11,5 +13,7 @@ module.exports = {
   __STRING_ALPHABET_WITH_SPACE_RegExp__,
   __CONST_SEPARATOR__,
   __LENGTH_ALPHABET__,
-  __MAJUS_CODE_OF_A__
+  __MAJUS_CODE_OF_A__,
+  __DECRYPT__,
+  __ENCRYPT__
 };
