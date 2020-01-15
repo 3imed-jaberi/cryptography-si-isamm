5b733147e4387621538de2297db1c5eedf8f0072
3imed-jaberi <imed_jebari@hotmail.fr>
2020-01-15 18:26:54 +0100

    clean-up the vignere 🪒..

--- src/cryptography/Vigenere.js
+++ src/cryptography/Vigenere.js
@@ -1,6 +1,12 @@
 const { reformKey } = require('../utils');
-const { __STRING_ALPHABET_RegExp__, __MAJUS_CODE_OF_A__, __LENGTH_ALPHABET__  } = require('../global');
 const { isString } = require('../validation');
+const { 
+  __STRING_ALPHABET_RegExp__,
+  __MAJUS_CODE_OF_A__, 
+  __LENGTH_ALPHABET__,
+  __ENCRYPT__,
+  __DECRYPT__
+} = require('../global');
 
 
 /**
@@ -12,26 +18,27 @@ const { isString } = require('../validation');
  * @param {string} type : type of operation ..
  */
 const main = (message, key, type) => {
-
   if(isString(message) && isString(key) && isString(type)) {
 
     let cleanUpMsg = message.toUpperCase().match(__STRING_ALPHABET_RegExp__);
-
     let source = cleanUpMsg.map( letter => letter.charCodeAt(0) - __MAJUS_CODE_OF_A__);
       
     let reformTheKey= reformKey(key,source.length).toUpperCase().split("").map(letter => letter.charCodeAt(0) - __MAJUS_CODE_OF_A__);
     
-    
-    if ( type.toLowerCase() === 'encrypt' ){
-      return source.map( (num,index) => String.fromCharCode((num + reformTheKey[index]) % __LENGTH_ALPHABET__ + __MAJUS_CODE_OF_A__) ).join("");
-  
-    }else if ( type.toLowerCase() === 'decrypt' ){
-      return source.map( (num,index) => {
-        let charAssertDecode = num - reformTheKey[index] > 0 ? num - reformTheKey[index] : (num - reformTheKey[index])+26;
-        return String.fromCharCode( charAssertDecode % __LENGTH_ALPHABET__ + __MAJUS_CODE_OF_A__)
-      }).join("");
-    }else{
-      throw new Error('Your type should be `decrypt` or `encrypt` .. ')
+    switch (type.toLowerCase()) {
+      case __ENCRYPT__: {
+        return source.map((num, index) => String.fromCharCode((num + reformTheKey[index]) % __LENGTH_ALPHABET__ + __MAJUS_CODE_OF_A__)).join('');
+      }
+      case __DECRYPT__: {
+        return source.map((num, index) => {
+                  let charAssertDecode = num - reformTheKey[index] > 0 ? num - reformTheKey[index] : (num - reformTheKey[index]) + 26;
+
+                  return String.fromCharCode(charAssertDecode % __LENGTH_ALPHABET__ + __MAJUS_CODE_OF_A__)
+               }).join('');
+      } 
+      default: {
+        throw new Error('Your type should be `decrypt` or `encrypt` .. ');
+      }
     }
   }else{
     throw new Error('Check you inputs .. ');
@@ -39,4 +46,4 @@ const main = (message, key, type) => {
 };
 
 
-module.exports = main ;
+module.exports = main;
